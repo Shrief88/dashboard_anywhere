dcfdfb2f2b01d22257f2ff70a933229e408205d4
Shrief Essam <shriefessam1999@gmail.com>
2023-11-28 13:52:47 +0200

    edit fields in instructor model

--- src/models/Instructor.ts
+++ src/models/Instructor.ts
@@ -1,15 +1,23 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 export interface IInstructor extends Document {
-  name: string;
+  first_name: string;
+  family_name : string;
   position: string;
   photoUrl: string;
+  Gender : string;
 }
 
 const instructorSchema: Schema = new Schema({
-  name: { type: String, required: true },
+  first_name: { type: String, required: true },
+  family_name: { type: String, required: true },
   position: { type: String, required: true, unique: true },
   photoUrl: { type: String, required: true },
+  Gender: { type: String, required: true },
+});
+
+instructorSchema.virtual("fullName").get(function () {
+  return `${this.first_name} ${this.family_name}`;
 });
 
 instructorSchema.virtual("url").get(function(){
