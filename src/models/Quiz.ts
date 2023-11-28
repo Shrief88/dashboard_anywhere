9237d6f7313f699a50206182aa1b898710097d2f
Shrief Essam <shriefessam1999@gmail.com>
2023-11-28 14:30:28 +0200

    update quiz model

--- src/models/Quiz.ts
+++ src/models/Quiz.ts
@@ -16,9 +16,9 @@ const QuizSchema: Schema = new Schema({
   due_to_hour: { type: Number, required: true },
 });
 
-// QuizSchema.virtual("url").get(function () {
-//   return `/quiz/${this.id}`;
-// });
+QuizSchema.virtual("url").get(function () {
+  return `/quiz/${this.id}`;
+});
 
 export const QuizModel = mongoose.model<IQuiz>("Quiz", QuizSchema);
 
