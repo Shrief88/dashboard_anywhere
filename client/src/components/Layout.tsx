44d96555bf437bf7d16b61186fbacabff06edb6b
Shrief Essam <shriefessam1999@gmail.com>
2023-11-29 20:15:04 +0200

    solve bg-color in smaller screens

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -21,7 +21,7 @@ const Layout = () => {
     <div className='className="relative flex flex-row w-full h-full overflow-hidden"'>
       <SideBar onExpand={handleExpand} onMobile={handleMobile} />
       <main
-        className={`transition-all transform duration-100 text-slate-700 bg-slate-100 w-full${
+        className={`transition-all transform duration-100 text-slate-700 w-full bg-slate-100 ${
           expanded ? 'md:ml-64' : 'md:ml-20'
         }`}
       >
@@ -34,28 +34,30 @@ const Layout = () => {
             className="md:hidden z-40"
           />
         )}
-        <header className="flex flex-row sticky top-0 items-center bg-white border-b border-b-gray-100 w-full shadow-sm min-h-[4rem]">
-          <NavBar />
+        <div className=" bg-slate-100 w-full">
+          <header className="flex flex-row sticky top-0 items-center bg-white border-b border-b-gray-100 w-full shadow-sm min-h-[4rem]">
+            <NavBar />
 
-          <Button
-            onClick={() => {
-              sidebar.toggleMobile();
-            }}
-            size="sm"
-            color="white"
-            icon
-            className="ml-auto flex md:hidden"
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
-              <path d="M448 96c0-17.7-14.3-32-32-32H32C14.3 64 0 78.3 0 96s14.3 32 32 32H416c17.7 0 32-14.3 32-32zm0 320c0-17.7-14.3-32-32-32H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H416c17.7 0 32-14.3 32-32z" />
-              <path
-                className="opacity-50"
-                d="M0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32z"
-              />
-            </svg>
-          </Button>
-        </header>
-        <Outlet />
+            <Button
+              onClick={() => {
+                sidebar.toggleMobile();
+              }}
+              size="sm"
+              color="white"
+              icon
+              className="ml-auto flex md:hidden"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512">
+                <path d="M448 96c0-17.7-14.3-32-32-32H32C14.3 64 0 78.3 0 96s14.3 32 32 32H416c17.7 0 32-14.3 32-32zm0 320c0-17.7-14.3-32-32-32H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H416c17.7 0 32-14.3 32-32z" />
+                <path
+                  className="opacity-50"
+                  d="M0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32z"
+                />
+              </svg>
+            </Button>
+          </header>
+          <Outlet />
+        </div>
       </main>
     </div>
   );
